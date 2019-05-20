b5eda758c3bfb21666b857ce0ccb1978368f5e07
Vinicius Berrios Rodriguez Almeida <50068691866@info.local>
2019-05-20 17:21:54 -0300

    acabei praticamente

--- MedicalGroupMobile/src/pages/cadastrar.js
+++ MedicalGroupMobile/src/pages/cadastrar.js
@@ -1,10 +1,46 @@
 import React,{Component} from 'react';
 import {Text, Image, StyleSheet, View, FlatList, TouchableOpacity, AsyncStorage} from "react-native";
 import Topo from '../componentes/topo';
+import { TextInput } from 'react-native-gesture-handler';
 
 import api from '../services/api';
 
 class cadastrar extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            idProntuario:""
+            ,idMedico:""
+            ,dataConsulta:""
+            ,idSituacao:""
+            ,descricao:""
+        }
+    }
+    cadastrarConsulta = async() =>{
+        const token = await AsyncStorage.getItem("Sprint2Projeto");
+
+        fetch('http://192.168.3.114:5000/api/Consultas', {
+            method: 'POST',
+            body: JSON.stringify({
+                idProntuario: this.state.idProntuario,
+                idMedico: this.state.idMedico,
+                dataConsulta: this.state.dataConsulta,
+                idSituacao: this.state.idSituacao,
+                descricao: this.state.descricao
+            }),
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        })
+        .then(resposta => resposta.json())
+        .then(data => {
+            this.props.history.push('/listar');
+            console.log(data);
+        })
+        .catch(erro => console.log(erro))
+    }
+
     render(){
         return(
             <View>
@@ -15,13 +51,27 @@ class cadastrar extends Component{
 
             <View style={styles.quadradoo}>
                 <Text style={styles.tituloo}>Atendimento</Text>
+
+                <TextInput style={styles.input} onChangeText={prontuario=>this.setState({prontuario})} placeholder="Prontuário"/>
+
+                <TextInput style={styles.input} onChangeText={medico=>this.setState({medico})} placeholder="Médico"/>
+
+                <TextInput style={styles.input} onChangeText={dataConsulta=>this.setState({dataConsulta})} placeholder="Data da Consulta"/>
+
+                <TextInput style={styles.input} onChangeText={situacao=>this.setState({situacao})} placeholder="Situação"/>
+
+                <TextInput style={styles.input} onChangeText={descricao=>this.setState({descricao})} placeholder="Descrição"/>
+
+                <TouchableOpacity style={styles.button} onPress={this.cadastrar}>
+                    <Text style={styles.buttontxt}>Cadastrar</Text>
+                </TouchableOpacity>
             </View>
             
             </View>
         );
     }
-}
 
+}
 const styles = StyleSheet.create({
     fundo3:{
         height:550,
@@ -42,6 +92,33 @@ const styles = StyleSheet.create({
         fontSize:30,
         marginLeft:90,
         marginTop:10
+    },
+    input:{
+        height:40,
+        width:200,
+        borderWidth: 2,
+        borderColor: "black",
+        borderRadius:10,
+        marginLeft:77,
+        marginTop:22,
+        textAlign:"center"
+    },
+    button:
+    {
+        height:40,
+        width:120,
+        backgroundColor:"#61de65",
+        borderBottomColor:"black",
+        borderWidth:1,
+        borderRadius:10,
+        marginLeft:120,
+        marginTop:20
+    },
+    buttontxt:{
+        color:"white",
+        fontSize:17,
+        textAlign:"center",
+        marginTop:5
     }
 })
 
--- MedicalGroupMobile/src/pages/listar.js
+++ MedicalGroupMobile/src/pages/listar.js
@@ -1,56 +1,56 @@
-import React,{Component} from 'react';
-import{ Text, Image,StyleSheet, View, FlatList, AsyncStorage} from "react-native";
+import React, { Component } from 'react';
+import { Text, Image, StyleSheet, View, FlatList, AsyncStorage } from "react-native";
 import Topo from '../componentes/topo';
 import api from "../services/api";
 
-class listar extends Component{
+class listar extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            listaConsulta:[]
+        this.state = {
+            listaConsulta: []
         };
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.carregarConsultas();
     }
 
     carregarConsultas = async () => {
         const token = await AsyncStorage.getItem("Sprint2Projeto");
-        fetch('http://192.168.3.114:5000/api/Consultas',{
+        fetch('http://192.168.3.114:5000/api/Consultas', {
             headers: {
-                'Authorization' : 'Bearer ' + token
+                'Authorization': 'Bearer ' + token
             }
         })
-        .then(resposta => resposta.json())
-        .then(data => this.setState({listaConsulta : data}))
-        .catch(erro => console.log(erro))
+            .then(resposta => resposta.json())
+            .then(data => this.setState({ listaConsulta: data }))
+            .catch(erro => console.log(erro))
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <View>
-            <Topo />
-            
-            <View style={styles.quadrado3}>
-                <Text style={styles.tituluzinho}>Sua Consulta</Text>
+                <Topo />
 
-            </View>
+                <View style={styles.quadrado3}>
+                    <Text style={styles.tituluzinho}>Sua Consulta</Text>
 
-            <View style={styles.quadrado4}>
-                <FlatList
-                    data={this.state.listaConsulta}
-                    keyExtractor={item => item.id}
-                    renderItem={this.renderizaItem}
-                />
-            </View>
-            
+                </View>
+
+                <View style={styles.quadrado4}>
+                    <FlatList
+                        data={this.state.listaConsulta}
+                        keyExtractor={item => item.id}
+                        renderItem={this.renderizaItem}
+                    />
+                </View>
+
             </View>
         );
     }
 
-    renderizaItem = ({item}) => (
+    renderizaItem = ({ item }) => (
         <View>
             <View style={styles.alinha}>
                 <Text style={styles.Prontuario}>{"Prontuário:"}</Text>
@@ -82,95 +82,95 @@ class listar extends Component{
 }
 
 const styles = StyleSheet.create({
-    quadrado3:{
-        backgroundColor:"#2C914D",
+    quadrado3: {
+        backgroundColor: "#2C914D",
         borderRadius: 10,
-        height:50,
+        height: 50,
         width: 370,
         marginLeft: 20,
         marginTop: 35
     },
-    tituluzinho:{
-        textAlign:"center",
+    tituluzinho: {
+        textAlign: "center",
         color: "white",
-        fontSize:23,
-        marginTop:8
+        fontSize: 23,
+        marginTop: 8
     },
-    quadrado4:{
-        backgroundColor:"#2C914D",
-        borderRadius:10,
-        height:382,
-        width:370,
-        marginLeft:20,
-        marginTop:25
+    quadrado4: {
+        backgroundColor: "#2C914D",
+        borderRadius: 10,
+        height: 382,
+        width: 370,
+        marginLeft: 20,
+        marginTop: 25
     },
-    IdProntuarioo:{
-        color:"black",
-        fontSize:18,
-        marginLeft:130,
-        marginTop:-26
-    },
-    IdMedicoo:{
-        color:"black",
-        fontSize:18,
-        marginLeft:130,
-        marginTop:-26
-    },
-    dataConsultaa:{
-        color:"black",
-        fontSize:18,
-        marginLeft:130,
-        marginTop:-26
-    },
-    IdSituacaoo:{
-        color:"black",
-        fontSize:18,
-        marginLeft:130,
-        marginTop:-26
-    },
-    descricaoo:{
-        color:"black",
-        fontSize:18,
+    IdProntuarioo: {
+        color: "black",
+        fontSize: 18,
         marginLeft: 130,
-        marginTop:-26
+        marginTop: -26
     },
-    Prontuario:{
-        color:"white",
-        fontSize:20,
-        marginLeft:-210,
-        marginTop:10
+    IdMedicoo: {
+        color: "black",
+        fontSize: 18,
+        marginLeft: 130,
+        marginTop: -26
     },
-    Medico:{
-        color:"white",
-        fontSize:20,
-        marginLeft:-210,
-        marginTop:8
+    dataConsultaa: {
+        color: "black",
+        fontSize: 18,
+        marginLeft: 130,
+        marginTop: -26
     },
-    dataConsulta:{
-        color:"white",
-        fontSize:20,
-        marginLeft:-210,
-        marginTop:8
+    IdSituacaoo: {
+        color: "black",
+        fontSize: 18,
+        marginLeft: 130,
+        marginTop: -26
     },
-    situacao:{
-        color:"white",
-        fontSize:20,
-        marginLeft:-210,
-        marginTop:8
+    descricaoo: {
+        color: "black",
+        fontSize: 18,
+        marginLeft: 130,
+        marginTop: -26
     },
-    descricao:{
-        color:"white",
-        fontSize:20,
-        marginLeft:-210,
-        marginTop:8
+    Prontuario: {
+        color: "white",
+        fontSize: 20,
+        marginLeft: -210,
+        marginTop: 10
     },
-    alinha:{
-        alignItems:"center"
+    Medico: {
+        color: "white",
+        fontSize: 20,
+        marginLeft: -210,
+        marginTop: 8
     },
-    linha:{
+    dataConsulta: {
+        color: "white",
+        fontSize: 20,
+        marginLeft: -210,
+        marginTop: 8
+    },
+    situacao: {
+        color: "white",
+        fontSize: 20,
+        marginLeft: -210,
+        marginTop: 8
+    },
+    descricao: {
+        color: "white",
+        fontSize: 20,
+        marginLeft: -210,
+        marginTop: 8
+    },
+    alinha: {
+        alignItems: "center"
+    },
+    linha: {
         height: 5,
         width: 700,
-        backgroundColor:"white",
+        backgroundColor: "white",
         marginTop: 16,
     }
 
