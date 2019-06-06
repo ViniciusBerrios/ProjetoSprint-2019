f22985c7ea554b5913ba4604f9c9e7a0c106c5ec
Vinicius Berrios Rodriguez Almeida <50068691866@info.local>
2019-06-06 17:11:59 -0300

    terminando jaja

--- SpMedicalGroupReact/spmedicalgroupreact/src/Pages/DadosSistema/DadosSistema.js
+++ SpMedicalGroupReact/spmedicalgroupreact/src/Pages/DadosSistema/DadosSistema.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react';
 import firebase from '../../services/firebase';
+import Topo from '../../components/Topo/topo';
+import '../../Assets/css/dados.css';
 
 export default class DadosSistema extends Component{
     
@@ -42,20 +44,103 @@ export default class DadosSistema extends Component{
         this.listaDadosRealTime();
     }
 
+    atualizaEstado(event){
+        this.setState({[event.target.name] : event.target.value});
+    }
+
+    cadastrarDados(event){
+        event.preventDefault();
+
+        firebase.firestore().collection("analise")
+        .add({
+            Longitude: this.state.Longitude,
+            Latitude: this.state.Latitude,
+            IdadePaciente: this.state.IdadePaciente,
+            EspecialidadeMedico: this.state.EspecialidadeMedico,
+            DoencaPaciente: this.state.DoencaPaciente
+        }) .then(() =>{
+            alert("Dados Cadastrado Com Sucesso")
+            this.limparFormulario();
+        }).catch((erro) =>{
+            console.log('tag', erro);
+        })
+    }
+    limparFormulario(){
+        this.setState({
+            Longitude: '',
+            Latitude: '',
+            IdadePaciente: '',
+            EspecialidadeMedico: '',
+            DoencaPaciente: ''
+        })
+    }
+
     render(){
         return(
             <div>
 
-            <h1>Dados do Sistema</h1>
+            <Topo />
+            <section className="dados">
 
+            <div className="quadrado8">
+            
+            <h1>SISTEMA DOS PACIENTES</h1>
+
+
+            <div className="cadastrar">
+                <form onSubmit={this.cadastrarDados.bind(this)}>
+                
+                <div className="longitude_input">      
+                <input type="text" name="Longitude" placeholder="Longitude" value={this.state.Longitude} onChange={this.atualizaEstado.bind(this)}/>
+                </div>
+
+                <div className="latitude_input">
+                <input type="text" name="Latitude" placeholder="Latitude" value={this.state.Latitude} onChange={this.atualizaEstado.bind(this)}/>
+                </div>
+
+                <div className="idade_input">
+                <input type="text" name="IdadePaciente" placeholder="Idade do Paciente" value={this.state.IdadePaciente} onChange={this.atualizaEstado.bind(this)}/>
+                </div>
+
+                <div className="especialidade_input">
+                <input type="text" name="EspecialidadeMedico" placeholder="Especialidade do Médico" value={this.state.EspecialidadeMedico} onChange={this.atualizaEstado.bind(this)}/>
+                </div>
+
+                <div className="doenca_input">
+                <input type="text" name="DoencaPaciente" placeholder="Doença do Paciente" value={this.state.DoencaPaciente} onChange={this.atualizaEstado.bind(this)}/>
+                </div>
+
+                <div className="botazinho_enviar">
+                <button type="submit">Enviar</button>
+                </div>
+
+                    </form>
+            </div>
+
+            <div className="listagem2">
+
+            <h2>Lista de Dados</h2>
+
+            <table  id="tabela2">
             {
                 this.state.listaDados.map((analises) =>{
                     return(
-                        <li key= {analises.id}> {analises.Longitude} - {analises.Latitude} 
-                        - {analises.IdadePaciente} - {analises.EspecialidadeMedico} - {analises.DoencaPaciente}</li>
+                        <tr key= {analises.id}> 
+                        <td>{analises.Longitude}</td>
+                        <td>{analises.Latitude}</td> 
+                        <td>{analises.IdadePaciente}</td>
+                        <td>{analises.EspecialidadeMedico}</td>
+                        <td>{analises.DoencaPaciente}</td>
+                        </tr>
                     );
                 })
             }
+            </table>
+            </div>
+
+            </div>
+
+            </section>
 
             </div>
         )
